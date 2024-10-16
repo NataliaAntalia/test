451840144aa91c34d1e35a2398b6c23ee8f42675
Natalia <nataliaantaliaa@gmail.com>
2024-10-16 12:01:31 +0300

    Some commit from dev2

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React
         </a>
         <p>Test1</p>
+        <p>Some text1</p>
       </header>
     </div>
   );
